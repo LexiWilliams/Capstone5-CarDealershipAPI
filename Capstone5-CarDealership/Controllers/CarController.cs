6ca95c579b5958068377d0359308e099d9276ba8
Lexi <lexa.shelby@gmail.com>
2019-09-03 10:50:56 -0400

    Capstone 5 API first commit

---
+++ Capstone5-CarDealership/Controllers/CarController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Capstone5_CarDealership.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Capstone5_CarDealership.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class CarController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        private readonly CarDbContext _context;
+        public CarController(CarDbContext context)
+        {
+            _context = context;
+            if (_context.Cars.Count() == 0)
+            {
+                _context.Cars.Add(new Cars { Make = "Jeep", Model = "Grand Cherokee", Year = 1995, Color = "Blue" });
+            }
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Cars>>> GetCars()
+        {
+            var carList = await _context.Cars.ToListAsync();
+            return carList;
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Cars>> GetCarById(int id)
+        {
+            var found = await _context.Cars.FindAsync(id);
+            if (found == null)
+            {
+                return NotFound();
+            }
+            return found;
+        }
+        [HttpPost]
+        public async Task<ActionResult<Cars>> PostCar(Cars car)
+        {
+            _context.Cars.Add(car);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetCarById), new { id = car.Id }, car);
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Cars>> PutCar(int id, Cars car)
+        {
+            if (id != car.Id)
+            {
+                return BadRequest();
+            }
+            _context.Entry(car).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Cars>> DeleteCar(int id)
+        {
+            var car = await _context.Cars.FindAsync(id);
+            if (car == null)
+            {
+                return NotFound();
+            }
+            _context.Cars.Remove(car);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
---
+++ Capstone5-CarDealership/Models/CarDbContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Capstone5_CarDealership.Models
+{
+    public partial class CarDbContext : DbContext
+    {
+        public CarDbContext()
+        {
+        }
+
+        public CarDbContext(DbContextOptions<CarDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Cars> Cars { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=CarDb;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.6-servicing-10079");
+
+            modelBuilder.Entity<Cars>(entity =>
+            {
+                entity.Property(e => e.Color)
+                    .IsRequired()
+                    .HasMaxLength(30);
+
+                entity.Property(e => e.Make)
+                    .IsRequired()
+                    .HasMaxLength(30);
+
+                entity.Property(e => e.Model)
+                    .IsRequired()
+                    .HasMaxLength(30);
+            });
+        }
+    }
+}
